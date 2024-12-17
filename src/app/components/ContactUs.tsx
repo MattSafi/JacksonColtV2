2598b8ace4f2686dc697c9df24ecee15f2c740e0
MattSafi <matthewasafi@gmail.com>
2024-12-17 14:19:53 +0200

    contact us upgrades

--- src/app/components/ContactUs.tsx
+++ src/app/components/ContactUs.tsx
@@ -1,16 +1,24 @@
+import StarryBackground from "./StarryBackground";
+
 export default function ContactUs() {
   return (
     <>
-      <h2 className="text-3xl font-bold text-center">Contact Us</h2>
-      <p className="mt-4 text-lg text-center">
-        Do you want to get in touch with Jackson Colt?
-      </p>
-      <p className="mt-4 text-lg text-center">
-        You can send an email to jacksoncoltband@gmail.com
-      </p>
-      <p className="mt-4 text-lg text-center">
-        Or feel free to send us a message on our socials.
-      </p>
+      <div
+        className="max-w-lg mx-auto p-8 bg-background border-4 border-double rounded-lg shadow-lg
+        text-white"
+      >
+        <h2 className="text-4xl font-extrabold text-center mb-4">Contact Us</h2>
+        <p className="text-center text-lg">
+          Want to get in touch with{" "}
+          <span className="font-semibold">Jackson Colt</span>?
+        </p>
+        <p className="mt-6 text-center text-lg">
+          Email us at: <strong>jacksoncoltband@gmail.com</strong>
+        </p>
+        <p className="mt-6 text-center text-lg">
+          Or reach out through our socials below!
+        </p>
+      </div>
     </>
   );
 }
--- src/app/components/Parallax.tsx
+++ src/app/components/Parallax.tsx
@@ -5,24 +5,26 @@ export default function Parallax() {
       <div className="bg-parallax bg-fixed bg-center pt-10 bg-no-repeat h-screen"></div>
 
       {/* Content Section */}
-      <div className="p-20 border-2 rounded-lg">
-        <h1 className="text-4xl text-start font-extrabold mb-10">
+      <div className="p-10 md:p-20 bg-black border border-gray-200 rounded-md shadow-lg">
+        <h1 className="text-3xl font-extrabold text-white mb-6">
           Welcome to the Jackson Colt Website!
         </h1>
-        <p className="mt-4 text-start text-lg">
-          Jackson Colt embodies the spirit of 80s rock with a modern twist. His
-          music captures the essence of the golden age of guitar with infectious
-          choruses, melodic riffs, and electrifying solos. Influenced by classic
-          rock legends, Jackson blends retro vibes with a fresh, contemporary
-          sound that resonates across generations.{" "}
+        <p className="text-base text-white leading-relaxed mb-4">
+          Jackson Colt channels the raw energy of 80s rock, infusing it with a
+          fresh, modern edge. His music celebrates the golden era of
+          guitar-driven anthems, combining infectious choruses, soulful riffs,
+          and electrifying solos. Drawing inspiration from legendary rock icons,
+          Jackson seamlessly merges nostalgic influences with a contemporary
+          sound that transcends generations.
         </p>
-        <p className="mt-4 text-start text-lg">
-          His journey began at age 9 when he picked up his first guitar from a
-          second-hand store, learning tracks from icons like Creedence
-          Clearwater Revival, The Who, and Grand Funk Railroad. Growing up
-          immersed in the sounds of Deep Purple, AC/DC, Def Leppard, and Guns
-          N&apos; Roses, Jackson credits his family for shaping his timeless
-          rock-inspired style.
+        <p className="text-base text-white leading-relaxed">
+          His musical journey began at the age of nine when he discovered his
+          first guitar in a second-hand store. Self-taught and driven, he
+          mastered tracks from greats like Creedence Clearwater Revival, The
+          Who, and Grand Funk Railroad. Immersed in the powerful sounds of Deep
+          Purple, AC/DC, Def Leppard, and Guns N&apos; Roses, Jackson credits
+          his family&apos;s support for nurturing his timeless, rock-infused
+          style.
         </p>
       </div>
 
